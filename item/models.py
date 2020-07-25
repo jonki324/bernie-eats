3d0afc6bd8e3844df664b4b802c11ac73c8ec670
jonki324 <jonki324@gmail.com>
2020-07-26 01:40:22 +0900

    商品モデル追加

--- item/models.py
+++ item/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    class Meta:
+        db_table = 'items'
+
+    name = models.CharField(verbose_name='商品名', max_length=255)
+    price = models.IntegerField(verbose_name='価格')
+    comment = models.TextField(verbose_name='コメント')
+    img = models.BinaryField(verbose_name='画像')
